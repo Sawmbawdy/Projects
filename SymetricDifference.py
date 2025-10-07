6a3890023e57a0c268326b7c4d29988290d88abb
Sawmbawdy <saiompatro12@gmail.com>
2025-10-07 18:45:31 -0400

    Add files via upload

---
+++ SymetricDifference.py
@@ -0,0 +1,13 @@
+# part a
+set1 = {'blue', 'green'}
+set2 = {'blue', 'yellow'}
+# part b
+set3 = {1,2,3,4,5}
+set4 = {1,5,6,7,8,9}  
+
+#part a solve
+sym1and2 = set1.symmetric_difference(set2)
+print(sym1and2)
+#part b solve
+sym3and4 = set3.symmetric_difference(set4)
+print(sym3and4)
